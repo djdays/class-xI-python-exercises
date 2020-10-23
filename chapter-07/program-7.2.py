990d082383c11a628094241335a639ec8a962ccc
DJ Days <sridhanajeyan@gmail.com>
2020-10-23 15:29:15 +0530

    Add 3 Numbers

---
+++ chapter-07/program-7.2.py
@@ -0,0 +1,9 @@
+# Program 7.2
+# To add three numbers
+
+num1 = int(input("Enter number 1:  "))
+num2 = int(input("Enter number 2:  "))
+num3 = int(input("Enter number 3:  "))
+
+Sum  = num1 + num2 + num3
+print("Sum = ",Sum)
